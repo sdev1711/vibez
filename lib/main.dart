2b2d36515047bf7102f0dbadf224ed3cd790ba5c
sdev1711 <sonidev710@gmail.com>
2025-05-14 12:21:48 +0530

    run without flavour

--- lib/main.dart
+++ lib/main.dart
@@ -41,14 +41,43 @@ import 'firebase_options.dart';
 import 'generated/locales.g.dart';
 
 Future<void> initApp({Flavour? appFlavour}) async {
+  // await dotenv.load(fileName: "private_keys.env");
+  // WidgetsFlutterBinding.ensureInitialized();
+  // if (appFlavour == null) {
+  //   throw Exception("App flavor must not be null!");
+  // }
+  // await SharedPrefs.init();
+  // ThemeMode initialThemeMode = getStoredThemeMode();
+  // Config.appFlavour = appFlavour;
+  // Get.put(AppColors());
+  // String? languageCode = SharedPrefs.getLanguage();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  // await NotificationService.initializeNotifications();
+  // await StoryService().deleteExpiredStories();
+  // Locale initialCode = languageCode != null
+  //     ? Locale(languageCode, languageCode == 'en' ? 'US' : 'IND')
+  //     : Locale('en', 'US');
+  // Get.put(BottomNavController());
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((_) {
+  //   runApp(MyApp(
+  //     initialThemeMode: initialThemeMode,
+  //     initialLocale: initialCode,
+  //     initialRoute: AppRoutes.initialRoute,
+  //   ));
+  // });
+}
+void main()async{
   await dotenv.load(fileName: "private_keys.env");
   WidgetsFlutterBinding.ensureInitialized();
-  if (appFlavour == null) {
-    throw Exception("App flavor must not be null!");
-  }
+  // if (appFlavour == null) {
+  //   throw Exception("App flavor must not be null!");
+  // }
   await SharedPrefs.init();
   ThemeMode initialThemeMode = getStoredThemeMode();
-  Config.appFlavour = appFlavour;
+  // Config.appFlavour = appFlavour;
   Get.put(AppColors());
   String? languageCode = SharedPrefs.getLanguage();
   await Firebase.initializeApp(
